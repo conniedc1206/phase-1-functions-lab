06fe31d010b01ae3b3a74e2307090c13a9722476
Connie Park <conniedc1206@gmail.com>
2022-05-20 16:18:59 -0700

    commit tree 2078f3f833aeead4d04fefc13928ffea652bdbd9

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+//returns the number of blocks given a value
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= 42) {
+        return (someValue - 42)
+    } else if (someValue < 42) {
+        return (42 - someValue)
+    }
+}
+
+//Returns the number of feet from Scuber's headquarters to the pickup location. Use your distanceFromHqInBlocks function to help return the correct value here.
+function distanceFromHqInFeet(someValue) {
+    return (distanceFromHqInBlocks(someValue) * 264)
+}
+
+//Calculates the number of feet a passenger travels given a starting block and an ending block — it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
+function distanceTravelledInFeet(start, destination) {
+    if (destination >= start) {
+        return ((destination-start) * 264)
+    } else if (destination < start) {
+        return ((start-destination) * 264)
+    }
+  }
+
+function calculatesFarePrice(start, destination) {
+    
+    let totalTravelled = distanceTravelledInFeet(start, destination)
+    
+    if (totalTravelled <= 400) {
+        return 0;
+    } 
+    else if (totalTravelled > 400 && totalTravelled < 2000) {
+        return (totalTravelled- 400) * 0.02;
+    } 
+    else if (totalTravelled > 2000 && totalTravelled < 2500) {
+        return 25;
+    } 
+    else if (totalTravelled > 2500) {
+        return 'cannot travel that far';
+    }
+}
